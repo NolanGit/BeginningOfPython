007f3fa0582bcc07cd1924923861978c29ce2aa8
sunhaoran <sunhaoran@inspur.com>
2018-04-18 17:31:24 +0800

    2018/4/18

--- python基础教程/错误、调试和测试/调试.py
+++ python基础教程/错误、调试和测试/调试.py
@@ -1,6 +1,17 @@
 #coding=utf-8
+#断言assert
+'''
 def foo(n):
 	n=int(n)
-	assert n!=0,"n is zero"
+	assert n!=0,"n is zero"#n不等于0应该为True，当assert为False的情况下抛出AssertionError
 	return 10/n
 foo(0)
+'''
+#可以使用 -0 参数来关闭assert，关闭后所有的Assert语句都相当于pass
+import logging
+logging.basicConfig(level=logging.INFO)#配置信息，可以记录记录信息的级别
+s='0'
+n=int(s)
+logging.info('n=%d'%n)
+print(10/n)
+print('End')
