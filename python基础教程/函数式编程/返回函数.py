53c4243917fda9c1bb3d7e469542b490f2989819
sunhaoran <sunhaoran@inspur.com>
2018-04-25 19:37:40 +0800

    2018/4/25

--- python基础教程/函数式编程/返回函数.py
+++ python基础教程/函数式编程/返回函数.py
@@ -35,6 +35,15 @@ print(func1(),func2(),func3())
 
 #利用闭包返回一个计数器函数，每次调用它返回递增整数
 def createCounter():
+	i=[0]
 	def counter():
-		
+		i[0]+=1
+		return i[0]
 	return counter
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+	print('测试通过!')
+else:
+	print('测试失败!')
