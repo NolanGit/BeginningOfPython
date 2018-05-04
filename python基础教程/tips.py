f37ab34327edb3a541ed9773189e66dc8c1a29c3
sunhaoran <sunhaoran@inspur.com>
2018-05-04 11:37:19 +0800

    2018/5/4

--- python基础教程/tips.py
+++ python基础教程/tips.py
@@ -9,3 +9,18 @@ def set_gender(self,gender):
 			self.__gender=gender
 		else:
 			raise ValueError('Bad gender')
+
+#利用闭包返回一个计数器函数，每次调用它返回递增整数
+def createCounter():
+	i=[0]
+	def counter():
+		i[0]+=1
+		return i[0]
+	return counter
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+	print('测试通过!')
+else:
+	print('测试失败!')
