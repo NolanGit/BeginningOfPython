561ea692d27ce8c4bd7e02ad2d407e3a803e4795
sunhaoran <sunhaoran@inspur.com>
2018-05-18 10:43:43 +0800

    2018/5/18重写邮件发送方法

--- python发送多人邮件/MailWithPython.py
+++ python发送多人邮件/MailWithPython.py
@@ -3,16 +3,22 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 import time
-my_sender='XXXX@qq.com'
-my_pass = 'XXXX'
-my_user=['XXX@live.com','XXX@qq.com']
-msg=MIMEText('XXXX','plain','utf-8',)
-msg['From']=formataddr(["GoldMonitor",my_sender])
-msg['To']=formataddr(my_user)
-msg['Subject']="GoldPrice"
-server=smtplib.SMTP_SSL("smtp.qq.com", 465)
-server.login(my_sender, my_pass)
-server.sendmail(my_sender,my_user,msg.as_string())
-print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'邮件发送成功')
-server.quit()
-print('done')
+
+def MailSender(SenderName,ReceiverAddr,Subject,Content):
+	my_sender='XXXX@qq.com'
+	my_pass = 'XXXX'#口令，不是密码，通常为16位字符串
+	msg=MIMEText(Content,'plain','utf-8',)
+	msg['From']=formataddr([SenderName,my_sender])
+	msg['To']=formataddr(ReceiverAddr)
+	msg['Subject']=Subject
+	server=smtplib.SMTP_SSL("smtp.qq.com", 465)
+	server.login(my_sender, my_pass)
+	server.sendmail(my_sender,ReceiverAddr,msg.as_string())
+	server.quit()
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'邮件发送成功')
+
+ReceiverAddr=['XXX@live.com','XXX@qq.com']
+SenderName='SenderName'
+Subject='Subject'
+Content='TestContent'
+MailSender(SenderName,ReceiverAddr,Subject,Content)
