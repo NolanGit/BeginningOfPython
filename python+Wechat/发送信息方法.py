0814aef19147db4e918f2a9e304b44f2e89c969f
NolanSun <shr1213@live.com>
2018-05-19 11:35:19 +0800

    2018/5/19

--- python+Wechat/发送信息方法.py
+++ python+Wechat/发送信息方法.py
@@ -1,8 +1,9 @@
 import itchat
 itchat.auto_login(hotReload=True)
-users = itchat.search_friends(name = '海纯')
+users = itchat.search_friends(name = 'NickName')
 if users:
 	UserName=users[0]['UserName']
-	itchat.send('高兴啊',UserName)
+	itchat.send('XXX',UserName)
 else:
 	print('查无此人')
+#发送给文件传输助手itchat.send((time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))),'filehelper')
