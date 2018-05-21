9818c2c72184393b43756c0cbc41bb46fa3f8fc4
NolanSun <shr1213@live.com>
2018-05-21 21:37:14 +0800

    修正错误

--- python发送多人邮件/MailSender.py
+++ python发送多人邮件/MailSender.py
@@ -1,28 +1,29 @@
 #coding=utf-8
 import smtplib
+import time
 from email.mime.text import MIMEText
 from email.utils import formataddr
-import time
 
 class MailSender(object):
-	def __init__(self,my_sender,my_pass,SenderName,Subject,Content):
+
+	def __init__(self,my_sender,my_pass,sender_name,receiver_addr,subject,content):
 		self.my_sender=my_sender
 		self.my_pass=my_pass#口令，不是密码，通常为16位字符串
-		self.SenderName=SenderName
-		self.Subject=Subject
-		self.Content=Content
-	def SendIt():
-		my_sender=self.my_sender
-		my_pass =self.my_pass
-		msg=MIMEText(Content,'plain','utf-8',)
-		msg['From']=formataddr([SenderName,my_sender])
-		msg['To']=formataddr(ReceiverAddr)
-		msg['Subject']=Subject
+		self.sender_name=sender_name
+		self.receiver_addr=receiver_addr
+		self.subject=subject
+		self.content=content
+		
+	def send_it(self):
+		msg=MIMEText(self.content,'plain','utf-8',)
+		msg['From']=formataddr([self.sender_name,self.my_sender])
+		msg['Subject']=self.subject
 		server=smtplib.SMTP_SSL("smtp.qq.com", 465)
-		server.login(my_sender, my_pass)
-		server.sendmail(my_sender,ReceiverAddr,msg.as_string())
+		server.login(self.my_sender, self.my_pass)
+		server.sendmail(self.my_sender,self.receiver_addr,msg.as_string())
 		server.quit()
 		print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'邮件发送成功')
+
 
 my_sender='XXX@qq.com'
 my_pass='XXXXXX'
